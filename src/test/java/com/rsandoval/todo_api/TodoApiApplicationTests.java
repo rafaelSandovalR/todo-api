583bc38c81c4f02b75bbeea9b4f7210379af2996
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2025-11-06 10:13:24 -0500

    test: Adds initial web layer test for TaskController
    - Configures the test class with @WebMvcTest to test the controller class.
    - Injects `MockMvc` to perform mock HTTP GET requests.
    - Uses @MockitoBean to provide a mock `TaskRepository`
    - Implements the first test for GET /api/tasks, following the Arrange, Act, Assert (AAA) pattern to validate the JSON response.

--- src/test/java/com/rsandoval/todo_api/TodoApiApplicationTests.java
+++ src/test/java/com/rsandoval/todo_api/TodoApiApplicationTests.java
@@ -1,13 +1,54 @@
 package com.rsandoval.todo_api;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+// Testing only the TaskController
+@WebMvcTest(TaskController.class)
 class TodoApiApplicationTests {
+
+    // Spring will automatically inject ("autowire") our "fake postman" (MockMvc)
+    @Autowired
+    private MockMvc mockMvc;
+
+    // Spring will create a mock version of our repository
+    @MockitoBean
+    private TaskRepository taskRepository;
 
 	@Test
 	void contextLoads() {
 	}
+
+    @Test
+    void testGetAllTasks_ShouldReturnListOfTasks() throws Exception {
+        // -- ARRANGE-- Create fake data
+        Task task1 = new Task();
+        task1.setId(1L);
+        task1.setDescription("Read Dune Messiah");
+        task1.setCompleted(false);
+
+        // Teach the fake repository what to do
+        // WHEN taskRepository.findAll() is called, THEN return our fake list
+        Mockito.when(taskRepository.findAll()).thenReturn(List.of(task1));
+
+        // -- ACT -- Perform a fake GET request to API endpoint
+        mockMvc.perform(get("/api/tasks"))
+                // -- ASSERT -- (We expect the results to be)
+                .andExpect(status().isOk()) // a) An HTTP 200 OK status
+                // b) A JSON response; '$' is root of the JSON (the list), '[0]' is the first item in the list
+                .andExpect(jsonPath("$[0].description", is("Read Dune Messiah")))
+                .andExpect(jsonPath("$[0].completed", is(false)));
+    }
 
 }
