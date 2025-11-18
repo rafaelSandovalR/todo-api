9312a705cad861e491e732c96e9fd79554b28d44
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2025-11-18 16:57:45 -0500

    feat: Implement `JwtService` to generate a JWT
    - Extracts Digital Signature key from git-ignored `config.properties` file using helper `loadSecretKey()`.
    - Adds helper `getSignInKey()` to decode the base64 encoded key.
    - Implements `generateToken()` to build signed JWTs based on UserDetails.

---
+++ src/main/java/com/rsandoval/todo_api/JwtService.java
@@ -0,0 +1,55 @@
+package com.rsandoval.todo_api;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+@Service
+public class JwtService {
+    private static final String JWT_SECRET = loadSecretKey();
+
+    // Main method we will call when a user logs in
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+        return Jwts.builder()
+                .setClaims(extraClaims) // Add any custom data (optional)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis())) // "Created at" timestamp
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24)) // Expires in 24 hours
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256) // "Stamp" it with our secret key
+                .compact();
+    }
+
+    // Helper method to decode secret key
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(JWT_SECRET);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    // Extract "Digital Signature" key from config.properties
+    private static String loadSecretKey(){
+        Properties prop = new Properties();
+        try (FileInputStream input = new FileInputStream("config.properties")) {
+            prop.load(input);
+            String key = prop.getProperty("JWT_SECRET");
+            if (key == null || key.trim().isEmpty()) throw new RuntimeException("JWT_SECRET not found in config.properties");
+            return key;
+        } catch (IOException e) {
+            throw new RuntimeException("Could not load config.properties", e);
+        }
+    }
+}
