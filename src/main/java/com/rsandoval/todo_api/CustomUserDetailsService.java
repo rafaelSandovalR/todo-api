84701eaad5aaf3ebd52ffcec4e12d5e6a1c2fe89
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2025-11-18 12:13:36 -0500

    feat: Implements `CustomUserDetailsService` for database authentication
    - Creates a service annotated with @Service to bridge the custom `User` entity and Spring Security.
    - Implements `UserDetailsService` to load user data via the `UserRepository`.
    - Converts database entities into `UserDetails` objects for authentication.

---
+++ src/main/java/com/rsandoval/todo_api/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.rsandoval.todo_api;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // Find user in our database
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+
+        // Translate into a Spring Security "UserDetails" object
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getRole())
+                .build();
+    }
+}
